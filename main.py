1bfb19d67ff8cde9c1a549ff5f7236c57ef4b8d8
Черников Антон <avc105@tpu.ru>
2024-07-09 19:57:49 +0700

    start

---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+from my_token import API_TOKEN
+
+bot = telebot.TeleBot(API_TOKEN)
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет! Я ваш новый бот.")
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+# Запуск бота
+bot.polling()

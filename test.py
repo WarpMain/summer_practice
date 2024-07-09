6596855b43ec36b2d574c3044929bb3029e6a230
Черников Антон <avc105@tpu.ru>
2024-07-09 20:14:24 +0700

    start

--- test.py
+++ test.py
@@ -1,17 +1,37 @@
 import telebot
-from my_token import API_TOKEN
+from telebot import types
+from my_token import API_TOKEN  # Подставьте свой API токен сюда
 
 bot = telebot.TeleBot(API_TOKEN)
+
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "yo")
+    # Создание Inline Keyboard
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text='Кнопка 1', callback_data='button1')
+    button2 = types.InlineKeyboardButton(text='Кнопка 2', callback_data='button2')
+    keyboard.add(button1, button2)
+
+    # Отправка сообщения с Inline Keyboard
+    bot.reply_to(message, "Привет! Выберите одну из кнопок:", reply_markup=keyboard)
+
 
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
+
+# Обработчик callback-запросов от кнопок
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'button1':
+        bot.send_message(call.message.chat.id, 'Вы нажали на кнопку 1')
+    elif call.data == 'button2':
+        bot.send_message(call.message.chat.id, 'Вы нажали на кнопку 2')
+
 
 # Запуск бота
 bot.polling()
